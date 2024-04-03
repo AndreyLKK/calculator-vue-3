6c6f12d0f1029e36af66603d67b6661acef45868
AndreyLKK <awesomestim@gmail.com>
2024-04-04 00:39:53 +0700

    add js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="grid">
+    <input v-model="title" type="text" placeholder="0" />
+    <button
+      class="cell num"
+      v-for="num in numbers"
+      :key="num"
+      @click="addInput(num)"
+    >
+      {{ num }}
+    </button>
+    <button
+      class="cell op"
+      v-for="op in operations"
+      :key="op"
+      @click="addInput(op)"
+    >
+      {{ op }}
+    </button>
+    <button class="cell op" @click="clearInput">R</button>
+    <button class="cell op" @click="result">=</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "",
+      str: "",
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operations: ["+", "-", "*", "/"],
+    };
+  },
+
+  methods: {
+    addInput(char) {
+      this.title = this.str += char;
+      console.log(this.str);
+    },
+
+    result() {
+      this.title = eval(this.str);
+    },
+
+    clearInput() {
+      this.title = "";
+      this.str = "";
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: "Gilroy", sans-serif;
+  transition: 0.3s all;
+  font-weight: 600;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 200px;
+  box-shadow: 0 3px 15px grey;
+}
+input[type="text"] {
+  width: 200px;
+  border: 0;
+  color: #243849;
+  font-size: 18px;
+  padding: 5px 5px;
+  text-align: right;
+}
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+}
+.cell {
+  flex: 33%;
+  height: 50px;
+  font-size: 18px;
+  border: none;
+}
+.num {
+  background-color: #243849;
+  color: #fff;
+  font-weight: 400;
+}
+.cell:hover {
+  background-color: #182530;
+}
+.op {
+  background-color: #ff9900;
+  color: #243849;
 }
 </style>
